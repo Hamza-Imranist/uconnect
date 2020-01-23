9d87bb546391f43696ff4524838db85e8e72aa37
Fakhar Mahmood <fakharmahmood38@gmail.com>
2020-01-24 00:16:48 +0500

    integrated blog_app

--- Uconnect/urls.py
+++ Uconnect/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from Uconnect_app.views import index
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +25,6 @@ urlpatterns = [
     path('blog_app/', include('blog_app.urls', 'blog_app')),
     re_path(r'^$', index, name='index')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
