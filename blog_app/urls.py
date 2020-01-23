b1f6ce927cb77198e289bade13d427ce38ff22e2
Fakhar Mahmood <fakharmahmood38@gmail.com>
2020-01-23 23:11:27 +0500

    deleted old blog_app

--- blog_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import home
-
-app_name = 'blog_app'
-urlpatterns = [
-    path('', home, name='blog_home')
-]
--- blog_app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def home(request):
-    context_dict = {}
-    response = render(request, 'blog_app/Home.html', context_dict)
-    return response
